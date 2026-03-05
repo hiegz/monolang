7b4932f2b6e02923732f1185e4b677fc0bd10362
Volodymyr But <git@hiegz.com>
2026-03-05 18:04:12 +0100

    match numeric literals and operators

--- src/token.rs
+++ src/token.rs
@@ -4,9 +4,29 @@ use logos::Logos;
 #[logos(utf8 = true)]
 #[logos(skip r"[ \t\n\f]+")] // skip spaces, tabs, newlines, ...
 pub enum Token {
-    #[token("let")]
-    Let,
+    #[regex(r"-?\d+i8")]
+    Integer8,
+    #[regex(r"-?\d+i16")]
+    Integer16,
+    #[regex(r"-?\d+i32")]
+    Integer32,
+    #[regex(r"-?\d+i64")]
+    Integer64,
+    #[regex(r"-?\d+u8")]
+    Unsigned8,
+    #[regex(r"-?\d+u16")]
+    Unsigned16,
+    #[regex(r"-?\d+u32")]
+    Unsigned32,
+    #[regex(r"-?\d+u64")]
+    Unsigned64,
 
-    #[regex(r"[a-zA-Z_][a-zA-Z0-9_]*")]
-    Identifier,
+    #[token("+")]
+    Plus,
+    #[token("-")]
+    Minus,
+    #[token("/")]
+    Slash,
+    #[token("*")]
+    Star,
 }
